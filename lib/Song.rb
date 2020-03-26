3bdfb714b373f37b019934579f7988b51bab1c84
Tommy Connor <tconnor98@aol.com>
2020-03-26 16:44:55 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -2,3 +2,9 @@ class Song
   attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
   extend Concerns::Findable
   @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+end
